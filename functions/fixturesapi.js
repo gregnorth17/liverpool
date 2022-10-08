9461ed1dafb8f096ec8c764100fad14792bfab6c
gregnorth17 <greg_north@hotmail.co.uk>
2022-10-07 19:46:52 -0500

    netlify

--- functions/fixturesapi.js
+++ functions/fixturesapi.js
@@ -4,7 +4,7 @@ exports.handler = async (event, context) => {
 	const url = "https://v3.football.api-sports.io/fixtures?season=2022&team=40&from=2022-07-11&to=2023-05-28"
 	const {data} = await axios.get(url, {
 		headers: {
-			"x-apisports-key" : "db05fa99096787857df941d04fd5ec6e"
+			"x-apisports-key" : process.env.REACT_APP_API_KEY
 		}
 	})
 	try {
